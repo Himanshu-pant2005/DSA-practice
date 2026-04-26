b9d9402eb63b69076e36d884884c6394fc2369dc
Himanshu-pant2005 <panthimanshu39@gmail.com>
2026-04-26 12:18:52 +0530

    leetcode

---
+++ leetcode/two_sum_leetcode.java
@@ -0,0 +1,13 @@
+package leetcode;
+public class two_sum_leetcode {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1}; // Return an invalid index if no solution is found
+    }
+}
